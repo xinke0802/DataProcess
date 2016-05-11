31db38d0699bc3bf9b85f75e2a8e2d5240f73e54
Yangxin Zhong (MSR Student-Person Consulting) <v-yangxz@microsoft.com>
2016-05-11 22:30:35 +0800

    ms version

--- matlab/main.m
+++ matlab/main.m
@@ -1,12 +1,12 @@
 root = '..\DataProcess\bin\Release\';
 label = load([root, 'label_cluster.txt']);
 timeSim = load([root, 'clusterTimeSimilarity.txt']);
-hashtagSim = load([root, 'clusterWordJaccardSimilarity.txt']);
+hashtagSim = load([root, 'clusterTfIdfSimilarity.txt']);
 nameEntitySim = load([root, 'clusterNameEntitySetSimilarity.txt']);
 
-fid=fopen('jaccard\AdditionSimple_HierarchicalSingle.txt','w');
+fid=fopen('tfIdf\AdditionSimple_KmeansNormal.txt','w');
 fprintf(fid, 'clusterTimeSimilarity.txt\r\n');
-fprintf(fid, 'clusterWordJaccardSimilarity.txt\r\n');
+fprintf(fid, 'clusterTfIdfSimilarity.txt\r\n');
 fprintf(fid, 'clusterNameEntitySetSimilarity.txt\r\n');
 fprintf(fid, '\r\n');
 
@@ -21,25 +21,25 @@ for i = 0.0:0.1:1.0
 %             [labelE] = sc(A, 0, 686 + d);
 %             % endregion Spectral Clustering
             
-            % region Hierarchical Clustering
-            A = 1 - A;
-            N = size(A, 1);
-            B = ones(1, N * (N - 1) / 2);
-            index = 1;
-            for ii = 1:1:N-1
-                for jj = ii+1:1:N
-                    B(index) = A(jj, ii);
-                    index = index + 1;
-                end
-            end
-            Z = linkage(B, 'ward');
-            labelE = cluster(Z, 'maxclust', 686 + d);
-                % moce: single, complete, average, weighted, ward
-            % endregion Hierarchical Clustering
+%             % region Hierarchical Clustering
+%             A = 1 - A;
+%             N = size(A, 1);
+%             B = ones(1, N * (N - 1) / 2);
+%             index = 1;
+%             for ii = 1:1:N-1
+%                 for jj = ii+1:1:N
+%                     B(index) = A(jj, ii);
+%                     index = index + 1;
+%                 end
+%             end
+%             Z = linkage(B, 'ward');
+%             labelE = cluster(Z, 'maxclust', 686 + d);
+%                 % moce: single, complete, average, weighted, ward
+%             % endregion Hierarchical Clustering
             
-%             % region Kmeans Clustering
-%             labelE = k_means(A, 'random', 686);
-%             % endregion Kmeans Clustering
+            % region Kmeans Clustering
+            labelE = k_means(A, 'random', 686);
+            % endregion Kmeans Clustering
 
 %             % region Kernel Kmeans Clustering
 %             [labelE] = knKmeans(A, 686, @knGauss);
